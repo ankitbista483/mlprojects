3576ff77dccde0e41b2c617d1abe12aa23c60f1b
AnkitBista <ankitbista1406@gmail.com>
2025-01-27 15:28:17 -0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -48,12 +50,11 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__ == "__main__":
-    print("Script is running...")
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
-    print(f"Train data saved at: {train_data}")
-    print(f"Test data saved at: {test_data}")
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-   
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
